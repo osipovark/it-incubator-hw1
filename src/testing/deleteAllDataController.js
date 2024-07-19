1493f5d349d6a89787c560e309c3b6ac437d6b4a
osipovark <osipovarkonst@gmail.com>
2024-07-19 20:34:06 +0300

    srdf

--- src/testing/deleteAllDataController.js
+++ src/testing/deleteAllDataController.js
@@ -1,6 +1,8 @@
 const db = require("../db/db");
 
 exports.deleteAllDataController = (req, res) => {
-  db = {};
+  for (const prop of Object.getOwnPropertyNames(db)) {
+    delete db[prop];
+  }
   res.sendStatus(204);
 };
